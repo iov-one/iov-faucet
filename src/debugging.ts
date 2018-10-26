8f914a55b5578cb412eddbf5404b17045d964bb1
Simon Warta <simon@kullo.net>
2018-10-26 16:01:59 +0200

    Fix token amount calculation

--- src/debugging.ts
+++ src/debugging.ts
@@ -2,7 +2,7 @@ import { BcpAccount, BcpCoin } from "@iov/bcp-types";
 
 /** A string representation of a coin in a human-readable format that can change at any time */
 export function debugCoin(coin: BcpCoin): string {
-  return `${coin.whole + coin.fractional / coin.sigFigs} ${coin.tokenTicker}`;
+  return `${coin.whole + coin.fractional / 10 ** coin.sigFigs} ${coin.tokenTicker}`;
 }
 
 /** A string representation of a balance in a human-readable format that can change at any time */
