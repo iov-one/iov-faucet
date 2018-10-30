a4aadef560b89c0205b3212fba243d32a01ad38e
Simon Warta <simon@kullo.net>
2018-10-30 08:53:30 +0100

    Add initial test setup

---
+++ src/codec.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+
+import { Codec, codecFromString } from "./codec";
+
+describe("Codec", () => {
+  it("can convert string to codec", () => {
+    expect(codecFromString("bns")).to.equal(Codec.Bns);
+    expect(codecFromString("lisk")).to.equal(Codec.Lisk);
+
+    expect(() => codecFromString("")).to.throw(/not supported/i);
+    expect(() => codecFromString("abc")).to.throw(/not supported/i);
+    expect(() => codecFromString("LISK")).to.throw(/not supported/i);
+  });
+});
