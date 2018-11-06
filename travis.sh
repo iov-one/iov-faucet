e4a83543ec77c4fb189e100a103199ef38286562
Simon Warta <simon@kullo.net>
2018-11-06 16:08:53 +0100

    Make travis.sh work locally

--- travis.sh
+++ travis.sh
@@ -7,22 +7,27 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 # Travis helpers
 #
 
-function fold_start() {
-  export CURRENT_FOLD_NAME="$1"
-  travis_fold start "$CURRENT_FOLD_NAME"
-  travis_time_start
-}
+if [[ "${TRAVIS_COMMIT:-}" != "" ]]; then
+  function fold_start() {
+    export CURRENT_FOLD_NAME="$1"
+    travis_fold start "$CURRENT_FOLD_NAME"
+    travis_time_start
+  }
 
-function fold_end() {
-  travis_time_finish
-  travis_fold end "$CURRENT_FOLD_NAME"
-}
+  function fold_end() {
+    travis_time_finish
+    travis_fold end "$CURRENT_FOLD_NAME"
+  }
+else
+  function fold_start() { true; }
+  function fold_end() { true; }
+fi
 
 #
 # Environment
 #
 
-TRAVIS_BUILD_VERSION=$(echo "${TRAVIS_COMMIT}" | cut -c 1-10)
+TRAVIS_BUILD_VERSION=$(echo "${TRAVIS_COMMIT:-}" | cut -c 1-10)
 BUILD_VERSION=${TRAVIS_BUILD_VERSION:-manual}
 
 #
@@ -75,7 +80,7 @@ docker run --read-only --rm "iov1/iov-faucet:${BUILD_VERSION}" help
 fold_end
 
 fold_start "dockerhub-upload"
-if [[ "$TRAVIS_NODE_VERSION" == "10" && "$TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ "${TRAVIS_NODE_VERSION:-}" == "10" && "${TRAVIS_OS_NAME:-}" == "linux" ]]; then
   # only run in one job of the build matrix
 
   if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_TAG" == "" ]] && [[ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]]; then
