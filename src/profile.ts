82a24e9d4a9079f0a026a19b431188504bdbcdca
Simon Warta <simon@kullo.net>
2019-04-29 14:48:14 +0200

    Show addresses and identity roles in setSecretAndCreateIdentities

--- src/profile.ts
+++ src/profile.ts
@@ -1,7 +1,7 @@
 import { ChainId } from "@iov/bcp";
 import { UserProfile } from "@iov/core";
 
-import { Codec } from "./codec";
+import { Codec, codecImplementation } from "./codec";
 import * as constants from "./constants";
 import { createWalletForCodec, faucetHdPath } from "./crypto";
 
@@ -9,22 +9,26 @@ export async function setSecretAndCreateIdentities(
   profile: UserProfile,
   mnemonic: string,
   chainId: ChainId,
-  blockchain: Codec,
+  codecName: Codec,
 ): Promise<void> {
   if (profile.wallets.value.length !== 0) {
     throw new Error("Profile already contains wallets");
   }
-  const wallet = profile.addWallet(createWalletForCodec(blockchain, mnemonic));
+  const wallet = profile.addWallet(createWalletForCodec(codecName, mnemonic));
 
   // first account is the token holder
   const numberOfIdentities = 1 + constants.concurrency;
   for (let i = 0; i < numberOfIdentities; i++) {
+    // create
     const purpose = 1229936198; // big endian of ascii "IOVF"
     const coin = constants.coinType;
     const instance = constants.instance;
-
-    console.log(`Creating identity m/${purpose}'/${coin}'/${instance}'/${i}' ...`);
     const path = faucetHdPath(purpose, coin, instance, i);
-    await profile.createIdentity(wallet.id, chainId, path);
+    const identity = await profile.createIdentity(wallet.id, chainId, path);
+
+    // log
+    const role = i === 0 ? "token holder " : `distributor ${i}`;
+    const address = codecImplementation(codecName).identityToAddress(identity);
+    console.log(`Created ${role} (m/${purpose}'/${coin}'/${instance}'/${i}'): ${address}`);
   }
 }
